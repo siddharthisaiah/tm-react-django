fc7bafc6152fdae48df3dae5b7ba9f4139589d8d
Siddharth <siddharthisaiah@gmail.com>
2019-05-22 22:32:13 +0530

    CRD functionality for leads with react and redux

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,19 +1,24 @@
-import React, { Component, Fragment } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
 
-import Header from './layout/Header';
-import Dashboard from './leads/Dashboard';
+import Header from "./layout/Header";
+import Dashboard from "./leads/Dashboard";
+
+import { Provider } from 'react-redux';
+import store from "../store";
 
 class App extends Component {
     render() {
 	return (
+	    <Provider store={store}>
 	    <Fragment>
 		<Header />
 		<div className="container">
 		<Dashboard />
 		</div>
-	</Fragment>
-	)
+	    </Fragment>
+	    </Provider>
+	);
     }
 }
 
